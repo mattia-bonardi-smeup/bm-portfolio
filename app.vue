6c2a91bd1fe2e9781e60bd72c2c71e84d1265ff4
Mattia Bonardi <72151183+mattia-bonardi-smeup@users.noreply.github.com>
2022-04-28 01:27:29 +0200

    Icon and navbar

--- app.vue
+++ app.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
+    <!-- Header -->
+    <div class="header-wrapper">
+    <header class="header">
+      <nav class="navbar">
+        <div class="logo-wrapper">
+          <a class="logo">Mattia Bonardi</a>
+        </div>
+        <div class="menu-wrapper">
+          <ul class="menu">
+            <li class="menu-item">ABOUT</li>
+            <li class="menu-item">CONTACTS</li>
+          </ul>
+          <div class="menu-button">
+            <i>MENU</i>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <icon icon="github"></icon>
+    </div>
     <NuxtPage />
   </div>
 </template>
---
+++ components/icon.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="icon" :style="iconStyle" @click="onClick"></div>
+</template>
+
+<script lang="ts">
+import { computed, ComputedRef, Ref, ref } from "vue";
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: null,
+    }
+  },
+  emits: ["iconClick"],
+  setup(props, context) {
+    const iconElement: Ref<HTMLElement> = ref();
+    const iconStyle: ComputedRef<string> = computed(() => {
+      return (
+        "-webkit-mask: url(/icons/" +
+        props.icon +
+        ".svg) center center no-repeat;"
+      );
+    });
+    const onClick = () => {
+      context.emit("iconClick");
+    };
+    return {
+      iconElement,
+      iconStyle,
+      onClick,
+    };
+  },
+};
+</script>
+
+<style>
+.icon {
+  background: #000;
+  height: 20px;
+  max-width: 20px;
+}
+</style>
