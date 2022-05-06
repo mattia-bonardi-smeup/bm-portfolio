a1059e52e0be753c7a3768c00d649c761d73aba8
Mattia Bonardi <mattiabonardi99@gmail.com>
2022-05-06 09:24:32 +0200

    Change color

--- pages/about.vue
+++ pages/about.vue
@@ -131,7 +131,7 @@
   margin: 0;
   padding: 0;
   border: 0;
-  background-color: rgb(22, 22, 22);
+  background-color: #121212;
 }
 
 .container{
--- pages/index.vue
+++ pages/index.vue
@@ -61,6 +61,7 @@ const toMail = () => window.open('mailto:mattiabonardi99@gmail.com');
   justify-content: center;
   opacity: 0.86;
   z-index: 5;
+  opacity: 0.9;
 }
 
 .title{
