b326feb013c1471411c6dbfa8ed435c94541ddad
Mattia Bonardi <mattiabonardi99@gmail.com>
2022-06-16 01:27:49 +0200

    Some fixes

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <div class="mesh-container">
-      <client-only>
-        <Pc></Pc>
-      </client-only>
+    <div ref="threeContainer" class="three-container">
     </div>
     <div ref="titleWrapper" class="title-wrapper">
         <h2 class="subtitle">I'm Mattia, a full stack</h2>
@@ -18,23 +15,71 @@
   </div>
 </template>
 
-<script lang="ts">
-export default {
-  components: {
-    Pc: () => {
-      if(process.client) {
-        return import('../components/pc.vue');
-      }
-    },
-  }
-}
-</script>
-
 <script setup lang="ts">
+import { onMounted } from "@vue/runtime-core";
+import * as THREE from "three";
+import { ref } from "vue";
+import SplineLoader from '@splinetool/loader';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"; 
+
 const toLinkedin = () => window.open("https://www.linkedin.com/in/mattia-bonardi-099b02235/","_blank");
 const toInstagram = () => window.open("https://www.instagram.com/mattiaabonardi/", "_blank");
 const toGitHub = () => window.open("https://github.com/mattiabonardi", "_blank");
 const toMail = () => window.open('mailto:mattiabonardi99@gmail.com');
+
+const threeContainer = ref();
+const titleWrapper = ref();
+
+onMounted(() => {
+	// camera
+	const camera = new THREE.OrthographicCamera(window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2,  -50000, 50000);
+	camera.position.set(40.96, 1.22, 1000);
+	camera.quaternion.setFromEuler(new THREE.Euler(0, 0, 0));
+
+	// scene
+	const scene = new THREE.Scene();
+
+	// spline scene
+	const loader = new SplineLoader();
+	loader.load(
+		'3d/pc.splinecode',
+		(splineScene) => {
+			scene.add(splineScene);
+		}
+	);
+
+	// renderer
+	const renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setAnimationLoop(animate);
+	threeContainer.value.appendChild(renderer.domElement);
+
+	// scene settings
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = THREE.PCFShadowMap;
+
+  renderer.setClearAlpha(1);
+  
+  // orbit controls
+	const controls = new OrbitControls(camera, titleWrapper.value);
+	controls.enableDamping = true;
+	controls.dampingFactor = 0.125;
+
+	window.addEventListener('resize', onWindowResize);
+	function onWindowResize() {
+		camera.left = window.innerWidth / - 2;
+		camera.right = window.innerWidth / 2;
+		camera.top = window.innerHeight / 2;
+		camera.bottom = window.innerHeight / - 2;
+		camera.updateProjectionMatrix();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+	}
+
+	function animate(time) {
+    renderer.render(scene, camera);
+    controls.update();
+	}
+	});
 </script>
 
 <style scoped>
@@ -42,7 +87,7 @@ body{
   pointer-events: none;
 }
 /** Background */
-.mesh-container{
+.three-container{
   position: fixed;
   z-index: -1;
   top: 0;
@@ -52,11 +97,9 @@ body{
   margin: 0;
   padding: 0;
   border: 0;
-  pointer-events: none;
 }
 
 .title-wrapper {
-  pointer-events: none;
   position: relative;
   padding: 0 75px 0;
   text-align: right;
@@ -75,7 +118,6 @@ body{
 }
 
 .title{
-  pointer-events: none;
   font-size: 40px;
   margin: 0;
   font-family: 'Lato', sans-serif;
@@ -83,7 +125,6 @@ body{
 }
 
 .subtitle{
-  pointer-events: none;
   font-size: 20px;
   font-family: 'Lato', sans-serif;
 }
